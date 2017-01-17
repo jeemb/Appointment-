6216618b4ca926cc9e0497e5b807abe91959e4c6
jb <jenniferbeem@gmail.com>
2017-01-16 18:32:41 -0800

    add mapping

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
 	$("#blanks form").submit(function(event) {
-      var name = $("input#nameInput").val();
-	    var date = $("input#dateInput").val();
-      var start = $("input#startInput").val();
-      var end = $("input#endInput").val();
-			var array = [name, date, start, end];
-			var arraynew = [];
-			arraynew.push(array[2],array[0]);
+			var blanks = ["name", "date", "start", "end"];
 
-      $(".nameInput").append(name);
-      $(".dateInput").append(date);
-      $(".startInput").append(start);
-      $(".endInput").append(end);
+			var uppercase = blanks.map(function(blank) {
+				return blank.toUpperCase();
+			})
+
+			blanks.forEach(function(blank) {
+				var userInput = $("input#" + blank).val();
+			});
+
+			// var name = $("input#nameInput").val();
+	    // var date = $("input#dateInput").val();
+      // var start = $("input#startInput").val();
+      // var end = $("input#endInput").val();
+			// var array = [name, date, start, end];
+			// // var arraynew = [];
+			// // arraynew.push(array[2],array[0]);
+			//
+      // $(".nameInput").append(name);
+      // $(".dateInput").append(date);
+      // $(".startInput").append(start);
+      // $(".endInput").append(end);
+
 
   $("#output").show();
 
