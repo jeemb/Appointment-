5443586daefc303c85f9e2a7f9f1075f350023fb
jb <jenniferbeem@gmail.com>
2017-01-16 18:04:59 -0800

    add looping

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 	    var date = $("input#dateInput").val();
       var start = $("input#startInput").val();
       var end = $("input#endInput").val();
+			var array = [name, date, start, end];
+			var arraynew = [];
+			arraynew.push(array[2],array[0]);
 
       $(".nameInput").append(name);
       $(".dateInput").append(date);
